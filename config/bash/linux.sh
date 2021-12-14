6f7d496b34363d153b994f218fe9997f1178d777
Elijan Mastnak <ejmastnak@gmail.com>
2021-12-14 17:42:54 -0500

    Quick commit to have lilypond ready for alux

--- config/bash/linux.sh
+++ config/bash/linux.sh
@@ -1,8 +1,14 @@
 # Additions to PATH
 # --------------------------------------------- #
-
 # For pip packages
 export PATH="$HOME/.local/bin:${PATH}"
 
 # For personal scripts
 export PATH="$HOME/scripts:${PATH}"
+
+# Environment variables
+# --------------------------------------------- #
+# For LilyPond inverse search
+export LYEDITOR="nvr --servername=/tmp/lilypondsocket +%(line)s %(file)s"
+
+# export LYEDITOR="nvim %(file)s"
