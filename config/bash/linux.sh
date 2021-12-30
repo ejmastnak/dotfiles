90af33d15129ab40069824810ca1f68e8b625ff6
Elijan Mastnak <ejmastnak@gmail.com>
2021-12-30 12:51:34 -0800

    Manage Ruby version with chruby on Linux

--- config/bash/linux.sh
+++ config/bash/linux.sh
@@ -11,6 +11,11 @@ export PATH="$HOME/scripts:${PATH}"
 export GEM_HOME="$(ruby -e 'puts Gem.user_dir')"
 export PATH="$PATH:$GEM_HOME/bin"
 
+# Manage Ruby version with chruby
+source /usr/local/share/chruby/chruby.sh
+source /usr/local/share/chruby/auto.sh
+chruby ruby-2.7.4
+
 # Environment variables
 # --------------------------------------------- #
 # For LilyPond inverse search
