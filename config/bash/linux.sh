22d83553ccb41e62e664df125d8e9ea511171f48
Elijan Mastnak <ejmastnak@gmail.com>
2021-12-30 11:57:01 -0800

    Add Ruby Gems to PATH on Linux

--- config/bash/linux.sh
+++ config/bash/linux.sh
@@ -7,6 +7,9 @@ export PATH="$HOME/.local/bin:${PATH}"
 export PATH="$HOME/scripts:${PATH}"
 # --------------------------------------------- #
 
+# To add Ruby Gems to PATH
+export GEM_HOME="$(ruby -e 'puts Gem.user_dir')"
+export PATH="$PATH:$GEM_HOME/bin"
 
 # Environment variables
 # --------------------------------------------- #
