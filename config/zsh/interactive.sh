d08768f99d20f60ca69280695685f3d629fbf20f
Elijan Mastnak <ejmastnak@gmail.com>
2022-04-30 20:32:13 -0400

    feat: refresh zcompdump only once per day

--- config/zsh/interactive.sh
+++ config/zsh/interactive.sh
@@ -1,2 +1,20 @@
 # Source generic interactive shell config
 . "${HOME}/.config/shell/interactive.sh"
+
+# Lines added by zsh-newuser-install
+HISTFILE=~/.histfile
+HISTSIZE=1000
+SAVEHIST=1000
+bindkey -v
+# End of lines added by zsh-newuser-install
+
+# Enable completion
+zstyle :compinstall filename '${HOME}/.zshrc'
+autoload -Uz compinit
+# Refresh zcompdump only once per day
+if [[ -n ~/.zcompdump(#qN.mh+24) ]]; then
+  compinit
+  touch ~/.zcompdump
+else
+  compinit -C
+fi
