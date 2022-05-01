f2ef10db452dca869794cf180583420fff357981
Elijan Mastnak <ejmastnak@gmail.com>
2022-04-30 20:38:21 -0400

    feat: add Vi-mode cursors

--- config/zsh/interactive.sh
+++ config/zsh/interactive.sh
@@ -5,7 +5,6 @@
 HISTFILE=~/.histfile
 HISTSIZE=1000
 SAVEHIST=1000
-bindkey -v
 # End of lines added by zsh-newuser-install
 
 # Enable completion
@@ -17,4 +16,31 @@ if [[ -n ~/.zcompdump(#qN.mh+24) ]]; then
   touch ~/.zcompdump
 else
   compinit -C
+fi
+
+
+# Set vi mode and configure cursor
+# Source https://github.com/lervag/dotfiles/blob/2e9a760897d5246c0f02e30c997b23f3894c688d/zsh/.zshrc#L241
+bindkey -v
+
+export KEYTIMEOUT=15
+insertmode () {
+  print -n '\e]12;#cccccc\a'
+  print -n '\e[6 q'
+}
+normalmode () {
+  print -n '\e]12;#e5e9f0\a'
+  print -n '\e[2 q'
+}
+
+# Update cursor when changing vi mode
+if [[ ! "$TERM" =~ 'linux' ]]; then
+  function zle-line-init zle-keymap-select {
+    case $KEYMAP in
+      (vicmd)      normalmode;;
+      (viins|main) insertmode;;
+    esac
+  }
+  zle -N zle-line-init
+  zle -N zle-keymap-select
 fi
