3bf511023d921cf46dce34cc77b7df18fc1023dd
Elijan Mastnak <ejmastnak@gmail.com>
2022-09-13 12:49:10 +0200

    Switch back to deprecated Illuminate version

--- config/nvim/lua/plugins/illuminate.lua
+++ config/nvim/lua/plugins/illuminate.lua
@@ -1,25 +1,34 @@
-vim.g.Illuminate_useDeprecated = 1
-vim.g.Illuminate_ftwhitelist = {'python, c'}
+vim.cmd [[
+  let g:Illuminate_useDeprecated = 1
+  let g:Illuminate_ftwhitelist = ['python, c']
+]]
 
-require'lspconfig'.jedi_language_server.setup {
-  on_attach = function(client)
-    require 'illuminate'.on_attach(client)
-  end,
-}
-require'lspconfig'.clangd.setup {
-  on_attach = function(client)
-    require 'illuminate'.on_attach(client)
-  end,
-}
--- require'lspconfig'.phpactor.setup{
+-- require('illuminate').configure({
+--   -- overrides allowlist
+--   -- filetypes_denylist = {'all'},
+--   filetypes_allowlist = {'c'},
+-- })
+
+-- require'lspconfig'.jedi_language_server.setup {
 --   on_attach = function(client)
 --     require 'illuminate'.on_attach(client)
 --   end,
---   root_dir = function(fname)
---       return require 'lspconfig'.util.root_pattern("composer.json", ".git", ".root")(fname) or vim.fn.getcwd()
---   end,
 -- }
 
-vim.api.nvim_command [[hi def link LspReferenceText CursorLine]]
-vim.api.nvim_command [[hi def link LspReferenceWrite CursorLine]]
-vim.api.nvim_command [[hi def link LspReferenceRead CursorLine]]
+-- require'lspconfig'.clangd.setup {
+--   on_attach = function(client)
+--     require 'illuminate'.on_attach(client)
+--   end,
+-- }
+-- -- require'lspconfig'.phpactor.setup{
+-- --   on_attach = function(client)
+-- --     require 'illuminate'.on_attach(client)
+-- --   end,
+-- --   root_dir = function(fname)
+-- --       return require 'lspconfig'.util.root_pattern("composer.json", ".git", ".root")(fname) or vim.fn.getcwd()
+-- --   end,
+-- -- }
+
+-- vim.api.nvim_command [[hi def link LspReferenceText CursorLine]]
+-- vim.api.nvim_command [[hi def link LspReferenceWrite CursorLine]]
+-- vim.api.nvim_command [[hi def link LspReferenceRead CursorLine]]
