58867dea54aed6dcdd4c14fc092729d7aaa6cfe6
Elijan Mastnak <ejmastnak@gmail.com>
2022-10-17 21:31:35 +0200

    Show relative path

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -11,7 +11,7 @@ require('lualine').setup {
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
+    lualine_c = {{'filename', path=1}},
     lualine_x = {'encoding', 'filetype'},
     lualine_y = {'progress'},
     lualine_z = {'location'}
@@ -19,7 +19,7 @@ require('lualine').setup {
   inactive_sections = {
     lualine_a = {},
     lualine_b = {},
-    lualine_c = {'filename'},
+    lualine_c = {{'filename', path=1}},
     lualine_x = {'location'},
     lualine_y = {},
     lualine_z = {}
