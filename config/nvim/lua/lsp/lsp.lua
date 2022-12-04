9293c499960d231ba335cd6b62deb88e127a5fa5
Elijan Mastnak <ejmastnak@gmail.com>
2022-12-04 17:41:25 +0100

    Create dedicated LSP config file

---
+++ config/nvim/lua/lsp/lsp.lua
@@ -0,0 +1,21 @@
+require('lspconfig').phpactor.setup{
+  cmd = {"phpactor", "language-server"},
+  filetypes = {"php"},
+  root_dir = require("lspconfig/util").root_pattern("composer.json", ".git"),
+  on_attach = function()
+    vim.diagnostic.disable(0)
+    require('illuminate').on_attach(client)
+  end
+}
+
+require('lspconfig').jedi_language_server.setup {
+  on_attach = function(client)
+    require('illuminate').on_attach(client)
+  end,
+}
+
+require('lspconfig').clangd.setup {
+  on_attach = function(client)
+    require('illuminate').on_attach(client)
+  end,
+}
