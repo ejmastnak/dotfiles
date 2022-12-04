58882d00b3f4d0c20c89a207d41b115b7ce3d121
Elijan Mastnak <ejmastnak@gmail.com>
2022-12-04 17:57:38 +0100

    Update pictures directory path

--- scripts/change-wallpaper.sh
+++ scripts/change-wallpaper.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 # Sets wallpaper on X display :0 to a random jpg file in my wallpapers directory
 
-directory="minimal-nature"
 directory="anime-nature"
 
-DISPLAY=:0 feh --no-fehbg --bg-fill --randomize ~/Pictures/wallpapers/${directory}/*.jpg
+DISPLAY=:0 feh --no-fehbg --bg-fill --randomize ~/Documents/pictures/media/wallpapers/${directory}/*.jpg
