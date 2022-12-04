be13883eeb634eab38ad60727e97dda016f259a0
Elijan Mastnak <ejmastnak@gmail.com>
2022-12-04 17:38:21 +0100

    Tweak run settings with ToggleTerm

--- config/nvim/ftplugin/sh.lua
+++ config/nvim/ftplugin/sh.lua
@@ -5,4 +5,4 @@ vim.bo.tabstop     = 2
 vim.bo.softtabstop = 2
 vim.bo.shiftwidth  = 2
 
-vim.keymap.set('', '<Leader>r', '<Cmd>TermExec cmd="sh %"<CR>')
+vim.keymap.set('', '<Leader>r', '<Cmd>Update<CR><Cmd>TermExec cmd="sh %"<CR>')
