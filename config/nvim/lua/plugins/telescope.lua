032a4eaa120b61482fbe3206f2810b5441f0e6e7
Elijan Mastnak <ejmastnak@gmail.com>
2022-12-04 17:45:43 +0100

    Add settings for Telescope and UltiSnips

---
+++ config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,10 @@
+-- require('telescope').setup{
+--   defaults = {
+--     file_ignore_patterns = {"^vendor/"}
+--   } 
+-- }
+
+vim.keymap.set('n', '<Leader>ff', '<Cmd>lua require("telescope.builtin").find_files()<CR>')
+vim.keymap.set('n', '<Leader>fj', '<Cmd>lua require("telescope.builtin").buffers()<CR>')
+vim.keymap.set('n', '<Leader>fg', '<Cmd>lua require("telescope.builtin").live_grep()<CR>')
+vim.keymap.set('n', '<Leader>f/', '<Cmd>lua require("telescope.builtin").current_buffer_fuzzy_find()<CR>')
