8214836a9288342fac7eb10fe5050f0bd4834620
Elijan Mastnak <ejmastnak@gmail.com>
2022-12-13 21:41:47 +0100

    Implement spawning all tasks with given tag

--- config/nvim/lua/plugins/toggletasks.lua
+++ config/nvim/lua/plugins/toggletasks.lua
@@ -2,12 +2,14 @@ require('toggletasks').setup { }
 require('telescope').load_extension('toggletasks')
 
 local discovery = require('toggletasks.discovery')
+
 local function spawn_by_name(name)
   local tasks = discovery.tasks():filter(function(task)
     return task.config.name == name
   end)
 
-  if #tasks > 0 then -- if any matching tasks found
+  if #tasks > 0 then
+    -- Span task with matching name
     tasks[1]:spawn()
     tasks[1].term:open()
   else
@@ -15,8 +17,27 @@ local function spawn_by_name(name)
   end
 end
 
+local function spawn_tasks_with_tag(tag)
+  local tasks = discovery.tasks():filter(function(task)
+    return vim.tbl_contains(task.config.tags or {}, tag)
+  end)
+
+  if #tasks > 0 then
+    -- Span all tasks with matching tag
+    for _, task in pairs(tasks) do
+      task:spawn()
+      task.term:open()
+    end
+  else
+    print("No matching tasks with tag", tag)
+  end
+
+end
+
 -- Open Telescope task picker
 vim.keymap.set('n', '<space>ts', require('telescope').extensions.toggletasks.spawn)
 
 -- Run "Project" task
 vim.keymap.set('n', '<Leader>tp', function() spawn_by_name("Project") end)
+
+vim.keymap.set('n', '<Leader>tg', function() spawn_tasks_with_tag("auto") end)
