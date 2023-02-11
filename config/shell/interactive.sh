b595ffac2b5c1db87804115936a5a84af4dc7dbd
Elijan Mastnak <ejmastnak@gmail.com>
2023-02-11 11:58:11 +0100

    Add latest shell aliases and shortcuts

--- config/shell/interactive.sh
+++ config/shell/interactive.sh
@@ -7,12 +7,25 @@ alias spu="sudo pacman -Syu"
 alias s="systemctl"
 
 # Back up selected documents to Google Drive with rclone
-alias backup_documents="rclone sync -v --filter-from ~/.config/rclone/filter.txt /home/ej/Documents/ google-drive:Documents/"
+alias backup_docs_to_google="rclone sync -v --filter-from ~/.config/rclone/filter.txt /home/ej/Documents/ google-drive:Documents/"
+
+alias backup_docs_to_mega="rclone sync -v --filter-from ~/.config/rclone/filter.txt /home/ej/Documents/ mega:Documents/"
 
 alias grep='grep -n --color=auto'
 
+# Recursive grep starting in current directory
+rgrep() {
+  if [ "$#" -eq 1 ]; then
+    grep -r "${1}" ./
+  fi
+}
+
 # Download and extract audio from YouTube videos
 alias dlp='yt-dlp -x'
+
+# Use loudgain to normalize opus or mp3 albums
+alias lg_mp3='loudgain -I3 -S -L -a -k -s e *.mp3'
+alias lg_opus='loudgain -a -k -s e *.opus'
 
 # bean-query command from Beancount
 alias bq='bean-query'
@@ -25,6 +38,10 @@ alias udpwr='udisksctl power-off --block-device'
 # improved ls
 alias l="exa"
 alias L="exa -la"
+
+# Jupyter notebook and lab
+alias jnb="jupyter notebook"
+alias jnl="jupyter lab"
 
 # Some Git alias
 alias gg="git status"
