a9588a1e162a3ed4879147640e584063c68adb05
Elijan Mastnak <ejmastnak@gmail.com>
2023-02-11 11:43:36 +0100

    Add latest plugin config tweaks

--- config/nvim/lua/plugins/rooter.lua
+++ config/nvim/lua/plugins/rooter.lua
@@ -1,2 +1,4 @@
 vim.g.rooter_patterns = {'.git', 'Makefile', 'package.json', '.root', 'build/env.sh'}
+
 vim.g.rooter_manual_only = 1
+vim.keymap.set('', '<Leader>tr', '<Cmd>Rooter<CR>')
--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -5,6 +5,7 @@
 -- }
 
 vim.keymap.set('n', '<Leader>ff', '<Cmd>lua require("telescope.builtin").find_files()<CR>')
+vim.keymap.set('n', '<Leader>fr', '<Cmd>Rooter<CR><Cmd>lua require("telescope.builtin").find_files()<CR>')
 vim.keymap.set('n', '<Leader>fj', '<Cmd>lua require("telescope.builtin").buffers()<CR>')
 vim.keymap.set('n', '<Leader>fg', '<Cmd>lua require("telescope.builtin").live_grep()<CR>')
 vim.keymap.set('n', '<Leader>f/', '<Cmd>lua require("telescope.builtin").current_buffer_fuzzy_find()<CR>')
