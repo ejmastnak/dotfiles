fbed577380f20c0f20716c8e6c83231905ceae34
Elijan Mastnak <ejmastnak@gmail.com>
2023-02-11 11:45:46 +0100

    Move all Luasnip helper funcs to one file (DRY)

---
+++ config/nvim/lua/personal/luasnip-helper-funcs.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+local ls = require("luasnip")
+local sn = ls.snippet_node
+local i = ls.insert_node
+
+function M.get_ISO_8601_date()
+  return os.date("%Y-%m-%d")
+end
+
+function M.get_visual(args, parent)
+  if (#parent.snippet.env.SELECT_RAW > 0) then
+    return sn(nil, i(1, parent.snippet.env.SELECT_RAW))
+  else
+    return sn(nil, i(1, ''))
+  end
+end
+
+return M
