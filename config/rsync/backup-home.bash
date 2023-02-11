c54d062a0cadea24b3afa3f132a6d7295ec71c00
Elijan Mastnak <ejmastnak@gmail.com>
2023-02-11 16:09:51 +0100

    Improve readability of rsync backup script

--- config/rsync/backup-home.bash
+++ config/rsync/backup-home.bash
@@ -6,6 +6,8 @@
 # 		backup-home.bash
 
 home="/home/ej"
+backup="/run/media/ej/Seagate/backups/ej"
+
 rsync_config_dir="${home}/.config/rsync/"
 include_list="home-backup-include.conf"
 exclude_list="home-backup-exclude.conf"
@@ -17,6 +19,17 @@ do
   [[ "${dir_to_backup}" = \#* ]] && continue  # skip commented lines
   echo "Backing up ${dir_to_backup}"
 
-  rsync -rptD --exclude-from="${home}/.config/rsync/${exclude_list}" "${home}/${dir_to_backup}" /mnt/seagate/backups/ej
+  # Options:
+  # --recursive recurse into subdirectories
+  # --perms preserve permissions
+  # --times preserve modification times
+  rsync \
+    --recursive \
+    --perms \
+    --times \
+    --exclude-from="${home}/.config/rsync/${exclude_list}" \
+    "${home}/${dir_to_backup}" "${backup}"
 
 done < "${rsync_config_dir}/${include_list}"
+
+date > "${backup}/BACKUP_DATE"
