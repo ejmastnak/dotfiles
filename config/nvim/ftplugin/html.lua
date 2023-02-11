c043f5648cc8de6eee3e104f3050857bef31bbe3
Elijan Mastnak <ejmastnak@gmail.com>
2023-02-11 11:39:07 +0100

    Add serve/view keybindings for HTML and Vue

--- config/nvim/ftplugin/html.lua
+++ config/nvim/ftplugin/html.lua
@@ -4,3 +4,23 @@ vim.bo.autoindent  = true
 vim.bo.tabstop     = 2
 vim.bo.softtabstop = 2
 vim.bo.shiftwidth  = 2
+
+-- function OpenInBrowser()
+--   vim.fn.jobstart("firefox " .. vim.fn.expand("%"))
+-- end
+--
+-- vim.keymap.set('', '<Leader>v', OpenInBrowser)
+
+-- Start npx serve
+vim.keymap.set('n', '<Leader>r', function()
+  vim.cmd('1TermExec cmd="npx serve"')
+  vim.cmd("wincmd k")    -- Return cursor to original window
+  vim.cmd("stopinsert")  -- Return to normal mode
+end)
+
+-- Open browser at localhost port 3000
+vim.keymap.set('n', '<Leader>v', function()
+  vim.cmd('2TermExec cmd="xdg-open http://localhost:3000/%"')
+  vim.cmd("wincmd k")    -- Return cursor to original window
+  vim.cmd("stopinsert")  -- Return to normal mode
+end)
--- config/nvim/ftplugin/vue.lua
+++ config/nvim/ftplugin/vue.lua
@@ -1,6 +1,21 @@
 -- indentation settings
 vim.bo.expandtab   = true
 vim.bo.autoindent  = true
-vim.bo.tabstop     = 4
-vim.bo.softtabstop = 4
-vim.bo.shiftwidth  = 4
+vim.bo.tabstop     = 2
+vim.bo.softtabstop = 2
+vim.bo.shiftwidth  = 2
+
+
+-- Start npx serve
+vim.keymap.set('n', '<Leader>r', function()
+  vim.cmd('1TermExec cmd="npx serve"')
+  vim.cmd("wincmd k")    -- Return cursor to original window
+  vim.cmd("stopinsert")  -- Return to normal mode
+end)
+
+-- Open browser at localhost port 3000
+vim.keymap.set('n', '<Leader>v', function()
+  vim.cmd('2TermExec cmd="xdg-open http://localhost:3000/%"')
+  vim.cmd("wincmd k")    -- Return cursor to original window
+  vim.cmd("stopinsert")  -- Return to normal mode
+end)
