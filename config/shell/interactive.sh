36ecaf481000549b51225228ffaab7ce6aac94ff
Elijan Mastnak <ejmastnak@gmail.com>
2023-03-07 17:56:21 +0100

    Add display on/off functions

--- config/shell/interactive.sh
+++ config/shell/interactive.sh
@@ -20,6 +20,34 @@ rgrep() {
   fi
 }
 
+# Turn displays on
+display-on() {
+  if [[ "$#" -ne 1 ]]; then
+    exit
+  fi
+  if [[ ${1} -eq 0 ]]; then
+    xrandr --output eDP-1 --auto
+  elif [[ ${1} -eq 1 ]]; then
+    xrandr --output HDMI-1 --auto
+  elif [[ ${1} -eq 2 ]]; then
+    xrandr --output HDMI-2 --auto
+  fi
+}
+
+# Turn displays off
+display-off() {
+  if [[ "$#" -ne 1 ]]; then
+    exit
+  fi
+  if [[ ${1} -eq 0 ]]; then
+    xrandr --output eDP-1 --off
+  elif [[ ${1} -eq 1 ]]; then
+    xrandr --output HDMI-1 --off
+  elif [[ ${1} -eq 2 ]]; then
+    xrandr --output HDMI-2 --off
+  fi
+}
+
 # Download and extract audio from YouTube videos
 alias dlp='yt-dlp -x'
 
@@ -52,6 +80,9 @@ alias ggp="git push"
 
 # Some PHP/Laravel aliases
 alias art="php artisan"
+alias artmfs="php artisan migrate:fresh --seed"
+alias artt="php artisan tinker"
+alias artrl="php artisan route:list"
 
 # Network
 alias lswifi="nmcli device wifi list"
