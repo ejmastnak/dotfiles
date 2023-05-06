a1f2b2b2c74a3dad6fe9711f9e94b6efbebbf538
Elijan Mastnak <ejmastnak@gmail.com>
2023-05-05 20:56:55 -0400

    Add indent override keymappings

--- config/nvim/lua/personal/init/indent.lua
+++ config/nvim/lua/personal/init/indent.lua
@@ -1,5 +1,14 @@
-vim.bo.expandtab   = true
-vim.bo.autoindent  = true
-vim.bo.tabstop     = 2
-vim.bo.softtabstop = 2
-vim.bo.shiftwidth  = 2
+vim.o.expandtab   = true
+vim.o.autoindent  = true
+vim.o.tabstop     = 2
+vim.o.softtabstop = 2
+vim.o.shiftwidth  = 2
+
+-- For when Treesitter(?) overrides indentation
+vim.keymap.set('n', '<Leader>i', function()
+  vim.bo.expandtab   = true
+  vim.bo.autoindent  = true
+  vim.bo.tabstop     = 2
+  vim.bo.softtabstop = 2
+  vim.bo.shiftwidth  = 2
+end)
