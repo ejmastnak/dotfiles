bae51cda680d0513eadbc35753dcf2f74db16116
Elijan Mastnak <ejmastnak@gmail.com>
2023-05-05 20:56:35 -0400

    Switch to nondeprecated LuaSnip variable

--- config/nvim/lua/personal/luasnip-helper-funcs.lua
+++ config/nvim/lua/personal/luasnip-helper-funcs.lua
@@ -9,8 +9,8 @@ function M.get_ISO_8601_date()
 end
 
 function M.get_visual(args, parent)
-  if (#parent.snippet.env.SELECT_RAW > 0) then
-    return sn(nil, i(1, parent.snippet.env.SELECT_RAW))
+  if (#parent.snippet.env.LS_SELECT_RAW > 0) then
+    return sn(nil, i(1, parent.snippet.env.LS_SELECT_RAW))
   else
     return sn(nil, i(1, ''))
   end
