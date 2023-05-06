383513ae466dc12b0ccbe0d83a36cb34d950c4e6
Elijan Mastnak <ejmastnak@gmail.com>
2023-05-05 21:04:56 -0400

    Move java language server config to init.lua

--- config/nvim/ftplugin/java.lua
+++ config/nvim/ftplugin/java.lua
@@ -7,13 +7,3 @@ vim.bo.shiftwidth  = 4
 
 vim.keymap.set('', '<Leader>c', '<Cmd>update<CR><Cmd>TermExec cmd="javac %"<CR>')
 vim.keymap.set('', '<Leader>r', '<Cmd>update<CR><Cmd>TermExec cmd="java -cp %:h %:r"<CR>')
-
-local jdtls_config = {
-    cmd = {'/home/ej/.local/bin/jdtls'},
-    root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw', '.root'}, { upward = true })[1]),
-  on_attach = function(client)
-    vim.diagnostic.disable(0)
-    require('illuminate').on_attach(client)
-  end,
-}
-require('jdtls').start_or_attach(jdtls_config)
--- config/nvim/lua/lsp/lsp.lua
+++ config/nvim/lua/lsp/lsp.lua
@@ -1,4 +1,4 @@
-require('lspconfig').phpactor.setup{
+require('lspconfig').phpactor.setup {
   cmd = {"phpactor", "language-server"},
   filetypes = {"php"},
   root_dir = require("lspconfig/util").root_pattern("composer.json", ".git"),
@@ -10,6 +10,7 @@ require('lspconfig').phpactor.setup{
 
 require('lspconfig').jedi_language_server.setup {
   on_attach = function(client)
+    vim.diagnostic.disable(0)
     require('illuminate').on_attach(client)
   end,
 }
@@ -19,21 +20,31 @@ require('lspconfig').clangd.setup {
     require('illuminate').on_attach(client)
   end,
 }
-require('lspconfig').sumneko_lua.setup {
-  settings = {
-    Lua = {
-      runtime = {
-        version = 'LuaJIT',
-        -- version = 'Lua 5.1',
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
-  on_attach = function(client)
-    vim.diagnostic.disable(0)
-    require('illuminate').on_attach(client)
-  end,
-}
+
+-- require('jdtls').start_or_attach({
+--     cmd = {'/home/ej/.local/bin/jdtls'},
+--     root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw', '.root'}, { upward = true })[1]),
+--   on_attach = function(client)
+--     vim.diagnostic.disable(0)
+--     require('illuminate').on_attach(client)
+--   end,
+-- })
+
+-- require('lspconfig').sumneko_lua.setup {
+--   settings = {
+--     Lua = {
+--       runtime = {
+--         version = 'LuaJIT',
+--         -- version = 'Lua 5.1',
+--       },
+--       -- Do not send telemetry data containing a randomized but unique identifier
+--       telemetry = {
+--         enable = false,
+--       },
+--     },
+--   },
+--   on_attach = function(client)
+--     vim.diagnostic.disable(0)
+--     require('illuminate').on_attach(client)
+--   end,
+-- }
