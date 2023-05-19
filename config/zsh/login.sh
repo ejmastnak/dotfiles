80f5f0bd73869e6182ad5a3a677af7297fc69321
Elijan Mastnak <ejmastnak@gmail.com>
2023-05-19 17:01:27 +0200

    Increase saved ZSH history

--- config/zsh/login.sh
+++ config/zsh/login.sh
@@ -1,2 +1,17 @@
 # Source generic login shell config
 . "${HOME}/.config/shell/login.sh"
+
+# Set history size
+export HISTSIZE=10000
+
+# Save history after logout
+export SAVEHIST=10000
+
+# History file
+export HISTFILE=~/.zhistory
+
+# Append into history file
+setopt INC_APPEND_HISTORY
+
+# Save only one command if 2 common are same and consistent
+setopt HIST_IGNORE_DUPS
