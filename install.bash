3bb2f6f3bee0e9f01133780719486a77de4b3279
Elijan Mastnak <ejmastnak@gmail.com>
2023-06-04 15:59:37 +0200

    rename install script

--- install.bash
+++ install.bash
@@ -4,12 +4,12 @@
 
 BASEDIR="$(pwd)"
 
-# Long directories
-[[ ! -d "${HOME}/.config" ]]       && ln -s ${BASEDIR}/config ~/.config || echo '~/.config exists; skipping and not overwriting.'
-[[ ! -d "${HOME}/scripts" ]]       && ln -s ${BASEDIR}/scripts ~/scripts || echo '~/scripts exists; skipping and not overwriting.'
-[[ ! -d "${HOME}/.vim" ]]          && ln -s ${BASEDIR}/vim ~/.vim || echo '~/.vim exists; skipping and not overwriting.'
+# Link directories
+[[ ! -d "${HOME}/.config" ]] && ln -s ${BASEDIR}/config ~/.config || echo '~/.config exists; skipping and not overwriting.'
+[[ ! -d "${HOME}/scripts" ]] && ln -s ${BASEDIR}/scripts ~/scripts || echo '~/scripts exists; skipping and not overwriting.'
+[[ ! -d "${HOME}/.vim" ]]    && ln -s ${BASEDIR}/vim ~/.vim || echo '~/.vim exists; skipping and not overwriting.'
 
-# Link stand-alone files not in XDG_CONFIG
+# Link stand-alone files not inside config
 [[ ! -e "${HOME}/.bash_profile" ]] && ln -s ${BASEDIR}/bash_profile ~/.bash_profile || echo '~/.bash_profile exists; skipping and not overwriting.'
 [[ ! -e "${HOME}/.bashrc" ]]       && ln -s ${BASEDIR}/bashrc ~/.bashrc || echo '~/.bashrc exists; skipping and not overwriting.'
 [[ ! -e "${HOME}/.zprofile" ]]     && ln -s ${BASEDIR}/zprofile ~/.zprofile || echo '~/.zprofile exists; skipping and not overwriting.'
@@ -17,10 +17,9 @@ BASEDIR="$(pwd)"
 [[ ! -e "${HOME}/.inputrc" ]]      && ln -s ${BASEDIR}/inputrc ~/.inputrc || echo '~/.inputrc exists; skipping and not overwriting.'
 [[ ! -e "${HOME}/.xinitrc" ]]      && ln -s ${BASEDIR}/xinitrc ~/.xinitrc || echo '~/.xinitrc exists; skipping and not overwriting.'
 [[ ! -e "${HOME}/.xbindkeysrc" ]]  && ln -s ${BASEDIR}/xbindkeysrc ~/.xbindkeysrc || echo '~/.xbindkeysrc exists; skipping and not overwriting.'
-[[ ! -e "${HOME}/.xmodmaprc" ]]  && ln -s ${BASEDIR}/xmodmaprc ~/.xmodmaprc || echo '~/.xmodmaprc exists; skipping and not overwriting.'
-[[ ! -e "${HOME}/.XCompose" ]]  && ln -s ${BASEDIR}/XCompose ~/.XCompose || echo '~/.XCompose exists; skipping and not overwriting.'
+[[ ! -e "${HOME}/.xmodmaprc" ]]    && ln -s ${BASEDIR}/xmodmaprc ~/.xmodmaprc || echo '~/.xmodmaprc exists; skipping and not overwriting.'
+[[ ! -e "${HOME}/.XCompose" ]]     && ln -s ${BASEDIR}/XCompose ~/.XCompose || echo '~/.XCompose exists; skipping and not overwriting.'
 [[ ! -e "${HOME}/.gemrc" ]]        && ln -s ${BASEDIR}/gemrc ~/.gemrc || echo '~/.gemrc exists; skipping and not overwriting.'
-
 
 # If using ZSH as login shell, clone Zsh autosuggestions if not already installed
 if [[ "${SHELL}" == "/usr/bin/zsh" ]]; then
