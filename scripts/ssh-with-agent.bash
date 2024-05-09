2833739f418a6eb02423ca661973dad59addbbd3
Elijan Mastnak <git@ejmastnak.com>
2024-05-09 07:34:48 +0200

    fix: correct lookup of loaded identities

--- scripts/ssh-with-agent.bash
+++ scripts/ssh-with-agent.bash
@@ -27,18 +27,26 @@ get_identity_file() {
   ssh -G ${host} | awk '$1 == "identityfile" { print $2; exit }'
 }
 
+# Extract public key blob from an SSH public key file
+get_public_key_blob() {
+  local pubkey_file=${1}
+  head -1 "${pubkey_file}" | awk '{print $2}'
+}
+
 if [[ ${add_keys_to_agent} -gt 0 ]]; then
   # Original SSH command host
   SSH_HOST=${1}
-  SSH_ADD_TIMEOUT=3600  # seconds
+  SSH_ADD_TIMEOUT=36000  # seconds
 
   # Get the identity file for the host (and replace ~ with actual value of
   # ${HOME} to avoid problems with ~ not expanding in quotes down the line).
   IDENTITY_FILE=$(get_identity_file ${SSH_HOST})
   IDENTITY_FILE="${IDENTITY_FILE//\~/$HOME}"
+  PUBKEY_FILE="${IDENTITY_FILE}.pub"
+  PUBKEY_BLOB=$(get_public_key_blob "${PUBKEY_FILE}")
 
   # Check if this specific key is loaded in ssh-agent
-  if [[ -n "${IDENTITY_FILE}" ]] && ! ssh-add -l | grep -Fq "${IDENTITY_FILE}"; then
+  if [[ -n "${IDENTITY_FILE}" ]] && ! ssh-add -L | grep -Fq "${PUBKEY_BLOB}"; then
     echo "Key ${IDENTITY_FILE} for Host ${SSH_HOST} not loaded."
     read -p "Add key to ssh-agent? [Y/n]" response
     if [[ "${response}" == "n" ]]; then
@@ -48,6 +56,7 @@ if [[ ${add_keys_to_agent} -gt 0 ]]; then
       ssh-add -t ${SSH_ADD_TIMEOUT} ${IDENTITY_FILE}
     fi
   fi
+
 fi
 
 # Proceed with the original SSH command
